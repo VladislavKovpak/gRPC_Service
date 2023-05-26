c30f7d764c6038a0f738885af23c1a997d22648c
izzistudiodeveloper1 <developer1@izzi.studio>
2023-05-26 16:25:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using AwesomeService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc().AddJsonTranscoding();
+
+builder.Services.AddGrpcSwagger().AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "AwesomeService", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger().UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AwesomeService v1");
+});
+
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
