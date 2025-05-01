45906de0346d1b3431babaef31011c3f262d15ab
VladislavKovpak <81767676+VladislavKovpak@users.noreply.github.com>
2025-05-01 16:30:33 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ var app = builder.Build();
 app.UseSwagger().UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "AwesomeService v1");
-});
+}).RequireAuthorization();
 
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
